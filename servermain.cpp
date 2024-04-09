0c1181f255e7203624619eaf13f5b923a16e6eb7
Emmanuel Adebayo <adebayotokunbo@gmail.com>
2024-04-09 22:16:42 +0200

    Destination host and port no

--- servermain.cpp
+++ servermain.cpp
@@ -21,7 +21,7 @@ using namespace std;
 
 
 int main(int argc, char *argv[]){
-  if (argc != 2) {
+    if (argc != 2) {
         fprintf(stderr, "Usage: %s <ip>:<port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -30,9 +30,18 @@ int main(int argc, char *argv[]){
     Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
      Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
   */
-  char delim[]=":";
-  char *Desthost=strtok(argv[1],delim);
-  char *Destport=strtok(NULL,delim);
+    char delim[] = ":";
+    char *Desthost = strtok(argv[1], delim);
+    if (Desthost == NULL) {
+        fprintf(stderr, "Invalid address\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char *Destport = strtok(NULL, delim);
+    if (Destport == NULL) {
+        fprintf(stderr, "Invalid port\n");
+        exit(EXIT_FAILURE);
+    }
   // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
   // *Dstport points to whatever string came after the delimiter. 
 
