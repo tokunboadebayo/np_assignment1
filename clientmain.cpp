79fd2f6b2c660ca2fcdc23761bd0883340d8791f
Emmanuel Adebayo <adebayotokunbo@gmail.com>
2024-04-10 10:59:40 +0200

    Addd a message to receive

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+/* You will to add includes here */
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -9,25 +10,15 @@
 
 #include "calcLib.h"
 
-// Enable if you want debugging to be printed, see examble below.
-// Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
 #define DEBUG
 #define BUFFER_SIZE 256
-
-
-using namespace std;
-
 
 int main(int argc, char *argv[]){
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <ip>:<port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-  
-  /*
-    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
-     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
-  */
+
     char delim[] = ":";
     char *Desthost = strtok(argv[1], delim);
     if (Desthost == NULL) {
@@ -40,10 +31,7 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Invalid port\n");
         exit(EXIT_FAILURE);
     }
-  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-  // *Dstport points to whatever string came after the delimiter. 
 
-  /* Do magic */
   int port = atoi(Destport);
     if (port == 0) {
         fprintf(stderr, "Invalid port\n");
@@ -99,6 +87,13 @@ int main(int argc, char *argv[]){
 
     if (!protocolSupported) {
         fprintf(stderr, "Unsupported protocol\n");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *message = "OK\n";
+    if (send(sock, message, strlen(message), 0) == -1) {
+        perror("send");
         close(sock);
         exit(EXIT_FAILURE);
     }
