4106b04503df2ccdabfe3cd1cc2c358e9872cd2d
Emmanuel Adebayo <adebayotokunbo@gmail.com>
2024-04-16 22:30:23 +0200

    new modfication due to issues

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,6 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <netdb.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
 /* You will to add includes here */
 #include <sys/socket.h>
 #include <netinet/in.h>
